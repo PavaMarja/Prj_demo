44a3b2d1f06c7e79e2fddfc09efb8f4073915ef9
PavaMarja <48047684+PavaMarja@users.noreply.github.com>
2022-07-06 10:05:00 +0200

    Add files via upload

---
+++ resource/lang/hr/pagination.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'previous' => '&laquo; Prije',
+    'next'     => 'Idući &raquo;',
+];
---
+++ resource/lang/hr/passwords.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'password' => 'Lozinke moraju imati bar 6 karaktera i da se poklapaju.',
+    'reset'    => 'Lozinka je resetirana!',
+    'sent'     => 'Poslali smo vam email sa linkom za resetiranje lozinke!',
+    'token'    => 'Token za resetiranje1 lozinke je nevažeći.',
+    'user'     => 'Ne možemo naći korisnika sa tom email adresom.',
+    'updated'  => 'Vasa lozinka je promijenjena!',
+];
